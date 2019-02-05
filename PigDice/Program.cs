dfe00d590cd76e004203751fcc3232e2ec2665a8
chris <christopherscaudill@gmail.com>
2019-02-05 18:01:42 -0500

    Add project files.

---
+++ PigDice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PigDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
